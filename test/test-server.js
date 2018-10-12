bb8cdd85dc3bd62b46f6c373f7a818d30ac2190b
Josue Resilien <joshrez20@gmail.com>
2018-10-12 13:42:29 -0400

    Travis ci file added

--- test/test-server.js
+++ test/test-server.js
@@ -150,3 +150,72 @@ describe("Shopping List", function() {
     );
   });
 });
+
+describe("Recipes", function(){
+  it("should list items on GET", function(){
+    return chai.request(app)
+      .get("/recipes")
+      .then(function(res){
+        expect(res).to.be.json;
+        expect(res.body).to.be.a("array");
+        expect(res.body.length).to.be.at.least(1);
+
+        const expectedKeys = ['id','name','ingredients'];
+        res.body.forEach(function(item){
+          expect(item).to.be.a('object');
+          expect(item).to.include.keys(expectedKeys);
+        });
+      });
+  }); //it
+
+  it("should add an item on POST", function(){
+    const newItem={name:'Pasta',ingredients:["fettucine","alfredo","milk","cheese"]};
+    return chai.request(app)
+    .post("/recipes")
+    .send(newItem)
+    .then(function(res){
+      expect(res).to.have.status(201);
+      expect(res).to.be.json;
+      expect(res.body).to.be.a("object");
+      expect(res.body).to.include.keys("id","name","ingredients");
+      expect(res.body.id).to.not.equal(null);
+
+      expect(res.body).to.deep.equal(Object.assign(newItem, {id: res.body.id}));
+    });
+  }); //it
+
+  it("should update items on PUT", function(){
+    const updateData = {
+      id:2,
+      name:"Alfredo Pasta",
+      ingredients:["fettucine","alfredo","milk","cheese"]
+    };
+    return chai.request(app)
+    .get("/recipes")
+    .then(function(res){
+      updateData.id= res.body[0].id;
+      return chai.request(app)
+      .put(`/recipes/${updateData.id}`)
+      .send(updateData);
+    })
+    .then(function(res){
+      expect(res).to.have.status(204);
+    });
+  }); //it
+
+  it("Should delete items on DELETE", function(){
+    return chai.request(app)
+    .get("/recipes")
+    .then(function(res){
+      return chai.request(app)
+      .delete(`/shopping-list/${res.body[0].id}`);
+    })
+    .then(function(res){
+      expect(res).to.have.status(204);
+    });
+  }); //it 
+
+
+}); //describe end
+
+
